0f308fb279bbf604182ba2f00610f337bf012222
sorawit <thankzaki@hotmail.com>
2025-06-01 22:03:43 +0700

    Step 3: check internet connect or not?

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -4,6 +4,8 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
+import 'package:connectivity_plus/connectivity_plus.dart';
+
 class FirstScreen extends StatefulWidget {
   const FirstScreen({super.key});
 
@@ -17,6 +19,9 @@ class _FirstScreenState extends State<FirstScreen> {
   void initState() {
     super.initState();
 
+    // Step 3: check internet connect or not?
+    checkInternetConnection();
+
     // เมื่อครบเวลาจะทำการกดปุ่มโดยอัตโนมัติ
     Timer(
       const Duration(seconds: 3),
@@ -25,6 +30,41 @@ class _FirstScreenState extends State<FirstScreen> {
         MaterialPageRoute(builder: (context) => const SecondPage()),
       ),
     );
+  }
+
+  // Step 3: check internet connect or not?
+  // https://pub.dev/packages/connectivity_plus
+  // ใส่ async ให้รอ เพราะกว่ามือถือจะต่อเน็ตได้ มันไม่ได้ติดได้ในทันที
+  Future<void> checkInternetConnection() async {
+    var connectivityResult = await (Connectivity().checkConnectivity());
+
+    if (connectivityResult.contains(ConnectivityResult.mobile)) {
+      // Mobile network available.
+      print("Mobile network available.");
+    } else if (connectivityResult.contains(ConnectivityResult.wifi)) {
+      // Wi-fi is available.
+      // Note for Android:
+      // When both mobile and Wi-Fi are turned on system will return Wi-Fi only as active network type
+      print("Wi-fi network available.");
+    } else if (connectivityResult.contains(ConnectivityResult.ethernet)) {
+      // Ethernet connection available.
+      print("Ethernet (LAN) network available.");
+    } else if (connectivityResult.contains(ConnectivityResult.vpn)) {
+      // Vpn connection active.
+      // Note for iOS and macOS:
+      // There is no separate network interface type for [vpn].
+      // It returns [other] on any device (also simulator)
+      print("Vpn connection active.");
+    } else if (connectivityResult.contains(ConnectivityResult.bluetooth)) {
+      // Bluetooth connection available.
+      print("Bluetooth connection active.");
+    } else if (connectivityResult.contains(ConnectivityResult.other)) {
+      // Connected to a network which is not in the above mentioned networks.
+      print("Other connection active.");
+    } else if (connectivityResult.contains(ConnectivityResult.none)) {
+      // No available network types
+      print("No available network.");
+    }
   }
 
   @override
