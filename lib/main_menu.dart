04436f8ede472a31d779a1877ee7b90d796c4572
sorawit <thankzaki@hotmail.com>
2025-06-19 23:28:23 +0700

    Step 9: create a back button exit app popup

--- lib/main_menu.dart
+++ lib/main_menu.dart
@@ -1,4 +1,6 @@
 // Step 5: navigation bar
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 import 'package:crystal_navigation_bar/crystal_navigation_bar.dart';
@@ -32,42 +34,114 @@ class _Main_MenuState extends State<Main_Menu> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _pages[_selectedIdex],
-      bottomNavigationBar: CrystalNavigationBar(
-        backgroundColor: Colors.orange,
-        items: <CrystalNavigationBarItem>[
-          // index = 0;
-          CrystalNavigationBarItem(
-            icon: IconlyBold.home,
-            selectedColor: Colors.white, // โดนเลือกแล้วเป็นสีขาว
-            unselectedIcon: IconlyLight.home, // เวลาไม่เลือก ให้โปร่งใส
-            unselectedColor: Colors.black38,
-          ),
-          // index = 1;
-          CrystalNavigationBarItem(
-            icon: IconlyBold.document,
-            selectedColor: Colors.white,
-            unselectedIcon: IconlyLight.document,
-            unselectedColor: Colors.black38,
-          ),
-          // index = 2;
-          CrystalNavigationBarItem(
-            icon: IconlyBold.video,
-            selectedColor: Colors.white,
-            unselectedIcon: IconlyLight.video,
-            unselectedColor: Colors.black38,
-          ),
-          // index = 3;
-          CrystalNavigationBarItem(
-            icon: IconlyBold.location,
-            selectedColor: Colors.white,
-            unselectedIcon: IconlyLight.location,
-            unselectedColor: Colors.black38,
-          ),
-        ],
-        onTap: _onItemTapped,
-        currentIndex: _selectedIdex,
+    // Step 9: create a back button exit app popup
+    return WillPopScope(
+      onWillPop: () async {
+        final _Exit = await showDialog<bool>(
+          context: context,
+          builder:
+              (context) => AlertDialog(
+                title: const Text(
+                  'ออกจากแอพ',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.red,
+                    fontFamily: "Alike",
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                content: const Text(
+                  'คุณต้องการออกจากแอพหรือไม่?',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                    fontFamily: "Alike",
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                actions: <Widget>[
+                  Row(
+                    children: [
+                      ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor: WidgetStatePropertyAll(
+                            Colors.red.shade800,
+                          ),
+                        ),
+                        onPressed:
+                            () => Navigator.pop(context, false), // ไม่ออก,
+                        child: const Text(
+                          "ยกเลิก",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                            fontFamily: "Alike",
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                      const Spacer(),
+                      ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor: WidgetStatePropertyAll(
+                            Colors.green.shade800,
+                          ),
+                        ),
+                        onPressed: () => exit(0), // ออกแอพ
+                        child: const Text(
+                          "ออก",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                            fontFamily: "Alike",
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+        );
+        return _Exit ?? false; // ถ้าไม่เลือกอะไรเลย จะไม่ออกจากแอพ
+      },
+      child: Scaffold(
+        body: _pages[_selectedIdex],
+        bottomNavigationBar: CrystalNavigationBar(
+          backgroundColor: Colors.orange,
+          items: <CrystalNavigationBarItem>[
+            // index = 0;
+            CrystalNavigationBarItem(
+              icon: IconlyBold.home,
+              selectedColor: Colors.white, // โดนเลือกแล้วเป็นสีขาว
+              unselectedIcon: IconlyLight.home, // เวลาไม่เลือก ให้โปร่งใส
+              unselectedColor: Colors.black38,
+            ),
+            // index = 1;
+            CrystalNavigationBarItem(
+              icon: IconlyBold.document,
+              selectedColor: Colors.white,
+              unselectedIcon: IconlyLight.document,
+              unselectedColor: Colors.black38,
+            ),
+            // index = 2;
+            CrystalNavigationBarItem(
+              icon: IconlyBold.video,
+              selectedColor: Colors.white,
+              unselectedIcon: IconlyLight.video,
+              unselectedColor: Colors.black38,
+            ),
+            // index = 3;
+            CrystalNavigationBarItem(
+              icon: IconlyBold.location,
+              selectedColor: Colors.white,
+              unselectedIcon: IconlyLight.location,
+              unselectedColor: Colors.black38,
+            ),
+          ],
+          onTap: _onItemTapped,
+          currentIndex: _selectedIdex,
+        ),
       ),
     );
   }
