943e8aa2ab50cbbfb34c9d76fa319b8c8c5343e8
sorawit <thankzaki@hotmail.com>
2025-06-26 22:22:15 +0700

    / Step 13: for check firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:my_project/first_screen.dart';
 
-void main() {
+// Step 13: for check firebase
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+// Step 13: for check firebase
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
